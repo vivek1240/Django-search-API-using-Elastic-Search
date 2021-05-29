e877f4fece6e41faa9db34855b61c5320e6f68dd
vivek singh <38905088+vivek1240@users.noreply.github.com>
2021-05-29 18:39:48 +0530

    Revert "all files added"

--- internal_faq_es_api/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class InternalFaqEsApiConfig(AppConfig):
-    name = 'internal_faq_es_api'
--- internal_faq_es_api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import fetch_results
-
-urlpatterns= [
-    path('',fetch_results),
-    ]
